cf8b8b7ceb6969b062a42116cc995a2df5d8f5b6
Huking8 <huking850@gmail.com>
2024-12-10 13:27:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "LABy0TBS#nefAIM5HVamFOi8KMR6fTlf2T9iwLFEhVbSUdQeCDC4",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
